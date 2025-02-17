2ae532e01f84d5aa6c24e299c75ad3208cd67330
LilianaAmangoua <amangoua99@gmail.com>
2025-02-17 18:33:28 +0100

    Added contact page and router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './modules/home/components/home.component';
+import {AboutComponent} from './modules/about/components/about.component';
+import {ListComponent} from './modules/article/components/list/list.component';
+import {ArticleDetailsComponent} from './modules/article/components/article-details/article-details.component';
+import {ContactComponent} from './modules/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'articleList', component: ListComponent},
+  {path: 'article/:id', component: ArticleDetailsComponent},
+  {path: "contact", component: ContactComponent}
+];
--- src/app/modules/shared/components/menu/menu.component.ts
+++ src/app/modules/shared/components/menu/menu.component.ts
@@ -3,10 +3,11 @@ import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatIconButton} from '@angular/material/button';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbar, MatToolbarRow, MatIcon, MatMenuTrigger, MatMenu, MatMenuItem, MatIconButton],
+  imports: [MatToolbar, MatToolbarRow, MatIcon, MatMenuTrigger, MatMenu, MatMenuItem, MatIconButton, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
